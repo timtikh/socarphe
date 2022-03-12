301b2770b7fd5af97c93dbd1c5e4986c06e03323
timtikh <95489979+timtikh@users.noreply.github.com>
2022-03-12 18:21:40 +0300

    bot folder with new docker and stuff

---
+++ bot/app/SendResults.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import telebot
+
+
+if __name__ == "__main__":
+    with open("resultQueue.csv", "w") as file:
+        pass
+    with open("settings.txt") as file:
+        data = file.readlines()
+
+    token = data[2].strip()
+    bot = telebot.TeleBot(token)
+    trash = set()
+    while True:
+        try:
+            with open("resultQueue.csv") as file:
+                reader = csv.reader(file)
+                tg_id, user_status, users_res_list = None, None, None
+                for row in reader:
+                    requestId, tg_id, user_status, users_res_list = int(row[0]), row[1], row[2], row[3].split(";")
+                    if requestId in trash:
+                        continue
+                    else:
+                        trash.add(requestId)
+
+                    if user_status == "default":
+                        counter = 2
+                    else:
+                        counter = 5
+                    text = "*Найдены нужные пользователи!*\n\n\n"
+                    for el in users_res_list:
+                        user_vk_info = el.split("-")
+                        vk_id, match = user_vk_info[0], user_vk_info[1]
+                        if counter > 0:
+                            text += f"Совпадений: {match}\nСслыка: https://vk.com/id{vk_id}\n\n"
+                            counter += 1
+                        else:
+                            break
+                    if tg_id is not None:
+                        bot.send_message(int(tg_id), text, parse_mode='Markdown')
+        except FileNotFoundError:
+            pass
---
+++ bot/app/parseBotRes.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+import csv
+from ParsingVkApiRelis import *
+
+with open("settings.txt") as file:
+    data = file.readlines()
+    LOGIN = data[0].strip()
+    PASSWORD = data[1].strip()
+    depths = data[3].strip().split(',')
+
+vkBot = VKparserBot(login=LOGIN, password=PASSWORD)
+wfh = wordFindHelper()
+depth_depends_on_the_status = {"default": depths[0], "prime": depths[1], "superPrime": depths[2]}
+
+
+def service(users_list, user_status):
+    depth = int(depth_depends_on_the_status[user_status])
+    for el in users_list:
+
+        if el[0] not in trash:
+            trash.add(el[0])
+            tg_id = el[1]["tg_id"]
+            keywords = wfh.getKeyWords(el[1]["keywords"])
+            link = el[1]["link"]
+            if "https://vk.com" in link or "vk.com" in link:
+                vk_user_id = vkBot.vk.utils.resolveScreenName(
+                    screen_name=link.replace("https://vk.com/", "", 1).replace("vk.com/", "", 1))
+                if vk_user_id["type"] == "user":
+                    vkBot.findUserFriends(user_id=vk_user_id["object_id"], counter=0, depth=depth, keywords=keywords)
+                    result = []
+                    for vkItEl in vkBot.result_users.items():
+                        result.append((vkItEl[0], vkItEl[1]["count"]))
+                    with open("resultQueue.csv", "a", newline='') as file:
+                        writer = csv.writer(file)
+                        res = ""
+                        # result = [(97985038, 1), (132000826, 1), (210557817, 2), (225425107, 1)]
+                        for resEl in result:
+                            res += str(resEl[0]) + "-" + str(resEl[1]) + ";"
+                        print([el[0], tg_id, user_status, res[:-1]])
+                        writer.writerow([el[0], tg_id, user_status, res[:-1]])
+                    # print("result_users:", vkBot.result_users)
+                    # return vk_user_id["object_id"]
+                else:
+                    # print("Ты ввел ссылку не на пользователя Вконтакте\n[Type = {}]".format(vk_user_id["type"]))
+                    pass
+
+
+def readQueue():
+    with open("queue.csv") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            try:
+                users[row[2]][int(row[0])] = {"tg_id": int(row[1]), "keywords": row[3].split(";"), "link": row[4]}
+            except KeyError:
+                users[row[2]] = dict()
+                users[row[2]][int(row[0])] = {"tg_id": int(row[1]), "keywords": row[3].split(";"), "link": row[4]}
+    with open("queue.csv", "w") as file:
+        pass
+
+
+def saveQueue(users):
+    with open("queue.csv", "w") as file:
+        writer = csv.writer(file)
+        if len(users["prime"]) > 0:
+            for el in users["prime"].items():
+                queue_time, tg_id, status, keywords, link = el[0], el[1]["tg_id"], "prime", \
+                                                            ";".join(el[1]["keywords"]), el[1][link]
+                writer.writerow([queue_time, tg_id, status, keywords, link])
+        if len(users["default"]) > 0:
+            for el in users["default"].items():
+                queue_time, tg_id, status, keywords, link = el[0], el[1]["tg_id"], "default", \
+                                                            ";".join(el[1]["keywords"]), el[1][link]
+                writer.writerow([queue_time, tg_id, status, keywords, link])
+
+
+if __name__ == "__main__":
+    with open("queue.csv", "w") as file:
+        pass
+    trash = set()
+    while True:
+        users = {"default": dict(), "prime": dict()}
+
+        with open("queue.csv") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                try:
+                    users[row[2]][int(row[0])] = {"tg_id": int(row[1]), "keywords": row[3].split(";"), "link": row[4]}
+                except KeyError:
+                    users[row[2]] = dict()
+                    users[row[2]][int(row[0])] = {"tg_id": int(row[1]), "keywords": row[3].split(";"), "link": row[4]}
+
+        # Обслуживаем премиум пользователей
+        if len(users["prime"]) != 0:
+            users_list = sorted(list(users["prime"].items()), key=lambda x: x[0])
+            while len(users_list) > 0:
+                service(users_list, "prime")
+                # print(1, service(users_list, "prime"))
+                del users["prime"][users_list[0][0]]
+                try:
+                    del users_list[0]
+                except:
+                    break
+        # Обслуживаем обычных пользователей
+        elif len(users["default"]) != 0:
+            users_list = sorted(list(users["default"].items()), key=lambda x: x[0])
+            while len(users_list) > 0:
+                service(users_list, "default")
+                # print(0, service(users_list, "default"))
+                del users["default"][users_list[0][0]]
+                try:
+                    del users_list[0]
+                except:
+                    break
