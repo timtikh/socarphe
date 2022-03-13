9a54e8c2858e88ebcd0238010ec41e8679a729b2
timtikh <95489979+timtikh@users.noreply.github.com>
2022-03-13 19:15:11 +0300

    russian language added

--- bot/app/docker_entrypoint.sh
+++ bot/app/docker_entrypoint.sh
@@ -1,4 +1,4 @@
 # #!/bin/bash 
-python3 ./app/parseBotRes.py &
-python3 ./app/SendResults.py & 
-python3 ./app/telegram_bot_v.0.1.py
+PYTHONIOENCODING=utf-8 python3 ./app/parseBotRes.py &
+PYTHONIOENCODING=utf-8 python3 ./app/SendResults.py & 
+PYTHONIOENCODING=utf-8 python3 ./app/telegram_bot_v.0.1.py
