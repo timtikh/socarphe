0b1b4785d6019219b5b600f31b55b3b92b6e59cd
timtikh <95489979+timtikh@users.noreply.github.com>
2022-03-13 19:46:16 +0300

    some docker entry changes

--- bot/app/docker_entrypoint.sh
+++ bot/app/docker_entrypoint.sh
@@ -1,4 +1,6 @@
-# #!/bin/bash 
-PYTHONIOENCODING=utf-8 python3 ./app/parseBotRes.py &
-PYTHONIOENCODING=utf-8 python3 ./app/SendResults.py & 
-PYTHONIOENCODING=utf-8 python3 ./app/telegram_bot_v.0.1.py
+# #!/bin/bash/app
+python3 --version
+
+PYTHONIOENCODING=utf-8 python3 ./parseBotRes.py &
+PYTHONIOENCODING=utf-8 python3 ./SendResults.py & 
+PYTHONIOENCODING=utf-8 python3 ./telegram_bot_v.0.1.py &
