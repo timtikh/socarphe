480c9bce5f576bdd237f75f18c7a950306e78820
timtikh <95489979+timtikh@users.noreply.github.com>
2022-03-13 21:53:59 +0300

    fixed settings.txt bug
    all working docker

--- bot/app/docker_entrypoint.sh
+++ bot/app/docker_entrypoint.sh
@@ -1,6 +1,7 @@
-# #!/bin/bash/app
+#!/bin/bash/app
+cd app
 python3 --version
-
+ls
 PYTHONIOENCODING=utf-8 python3 ./parseBotRes.py &
 PYTHONIOENCODING=utf-8 python3 ./SendResults.py & 
-PYTHONIOENCODING=utf-8 python3 ./telegram_bot_v.0.1.py &
+PYTHONIOENCODING=utf-8 python3 ./telegram_bot_v.0.1.py 
