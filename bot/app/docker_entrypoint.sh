980a2e76696ced294e9b67b7632dc44b1bcc5990
timtikh <95489979+timtikh@users.noreply.github.com>
2022-03-15 01:38:14 +0300

    minor changes in bot

--- bot/app/docker_entrypoint.sh
+++ bot/app/docker_entrypoint.sh
@@ -2,9 +2,10 @@
 cd app
 python3 --version
 ls
+
 PYTHONIOENCODING=utf-8 python3 ./parseBotRes.py &
-PYTHONIOENCODING=utf-8 python3 ./SendResults.py & 
-PYTHONIOENCODING=utf-8 python3 ./telegram_bot_v.0.1.py 
+PYTHONIOENCODING=utf-8 python3 ./SendResults.py &
+PYTHONIOENCODING=utf-8 python3 ./telegram_bot.py 
 
 while 1>0
 do
@@ -16,9 +17,9 @@ ps -A | grep SendResults.py > /dev/null
 if [ $? = "1" ]
 then PYTHONIOENCODING=utf-8 python3 ./SendResults.py &
 fi
-ps -A | grep telegram_bot_v.0.1.py > /dev/null
+ps -A | grep telegram_bot.py > /dev/null
 if [ $? = "1" ]
-then PYTHONIOENCODING=utf-8 python3 ./telegram_bot_v.0.1.py &
+then PYTHONIOENCODING=utf-8 python3 ./telegram_bot.py &
 fi
 sleep 5
 done
